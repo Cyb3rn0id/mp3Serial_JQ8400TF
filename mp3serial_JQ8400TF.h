10f3c122015295b3184a04484e0f7939bee1351a
Giovanni (Gianni) Bernardo <settorezero@gmail.com>
2017-07-09 11:57:36 +0200

    Update mp3serial_JQ8400TF.h

--- mp3serial_JQ8400TF.h
+++ mp3serial_JQ8400TF.h
@@ -30,29 +30,29 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <stdint.h>
 
-#define	BYTE_START			0x7E
-#define BYTE_STOP			0xEF
+#define	BYTE_START		0x7E
+#define BYTE_STOP		0xEF
 
 // 1 bytes commands
-#define CMD_PLAY			0x01
-#define CMD_PAUSE			0x02
+#define CMD_PLAY		0x01
+#define CMD_PAUSE		0x02
 #define CMD_NEXT_SONG		0x03
 #define CMD_PREV_SONG		0x04
 #define CMD_VOLUME_UP   	0x05
 #define CMD_VOLUME_DOWN 	0x06
 #define CMD_FORWARD 		0x0A
 #define CMD_REWIND  		0x0B
-#define CMD_STOP 			0x0E
+#define CMD_STOP 		0x0E
 #define CMD_STOP_INJECT 	0x0F
 
 // 2 bytes commands
 #define CMD_SET_VOLUME 		0x31	// Set Volume: second byte is volume level: 0x00 to 0x30
 #define CMD_SEL_DEV 		0x35	// Select Device
-	#define DEV_TF 			0x01	// second of "Select Device" command: only Trans-Flash is selectable on this chip revision (JQ8400TF)
+	#define DEV_TF 		0x01	// second of "Select Device" command: only Trans-Flash is selectable on this chip revision (JQ8400TF)
 
 #define CMD_IC_MODE 		0x35
 	#define MODE_WAKE_UP 	0x02	// second byte of "IC Mode" command 	
-	#define MODE_SLEEP 		0x03	// ""	
+	#define MODE_SLEEP 	0x03	// ""	
 	#define MODE_RESET  	0x05	// ""
 
 // 3 bytes commands
@@ -67,7 +67,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 	#define PLAY_MODE_STUP		0x01	// ? don't works
 	#define PLAY_MODE_REPEAT	0x00	// ! warning: this is a 3 bytes command: 0x33, 0x00 and then the song index to repeat
 	
-#define CMD_PLAY_COMBINE 		0x45	// play combination up to 15 songs, followed by folder index and song index, up to 15. Example:
+#define CMD_PLAY_COMBINE 	0x45	// play combination up to 15 songs, followed by folder index and song index, up to 15. Example:
 										// 7E 08 45 01 05 02 01 01 03 EF : plays song 5 in folder 1, then song 1 in folder 2 and then song 3 in folder 1
 
 class mp3Serial
